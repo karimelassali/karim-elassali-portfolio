322fdaaf9396ccace3e5fd2b17c81df8d302c029
karim <fifakarim52@gmail.com>
2024-11-17 15:30:59 +0100

    Add favicons

--- app/layout.tsx
+++ app/layout.tsx
@@ -91,7 +91,10 @@ export default function RootLayout({
   return (
     <html lang="en" suppressHydrationWarning>
       <head>
-        <link rel="icon" href="/jsm-logo.png" sizes="any" />
+          <link rel="apple-touch-icon" sizes="180x180" href="favicon_io/apple-touch-icon.png" />
+          <link rel="icon" type="image/png" sizes="32x32" href="favicon_io/favicon-32x32.png" />
+          <link rel="icon" type="image/png" sizes="16x16" href="favicon_io/favicon-16x16.png" />
+          <link rel="manifest" href="/site.webmanifest"></link>
       </head>
       <body className={inter.className}>
         <ThemeProvider
